dfe5cedc07185008c4e44c5d62a6ffd7a6b6fa39
Laszlo Tarcsanyi <v-ltarcsanyi@hotels.com>
2016-08-10 11:45:30 +0200

    CKO-139: script to generate jvm args

---
+++ scripts/acceptance_test_jvm_args.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+#
+# Collect Bookingapp Acceptance test JVM args for IDE execution
+
+DEBUG="false"
+BA_HOME=""
+COPY_TO_CLIPBOARD="false"
+
+
+function print_help(){
+  echo "Usage:"
+  echo "${0} [-v] [-c] [-h] <bookingapp directory>"
+  echo "-v  verbose"
+  echo "-c  copy result to clipboard (Windows bash only, it uses /dev/clipboard)"
+  echo "-h  print this help"
+}
+
+function debug_log(){
+  [[ "${DEBUG}" == "true" ]] && echo "${1}" | awk '{printf("[DEBUG] %s\n", $0)}'
+}
+
+function error(){
+  echo "${1}" && print_help && exit 1
+}
+
+function get_maven_settings_file(){
+  local mvn_settings_files=$(mvn -X dummy | egrep "Reading (user|global) settings" | awk '{print $3, $6}')
+  local user_settings_file=$(echo "${mvn_settings_files}" | egrep "^user" | awk '{print $2}')
+  local global_settings_file=$(echo "${mvn_settings_files}" | egrep "^global" | awk '{print $2}')
+
+  if [ ! -z ${user_settings_file} ]; then
+    echo "${user_settings_file}"
+  else
+    echo "${global_settings_file}"
+  fi
+}
+
+function parse_maven_settings(){
+  cat ${1} \
+  | egrep "<(MVT_BUSINESS_CONFIGURATION_HOME|DIONYSUS_UIPACK_HOME)>" \
+  | awk -F'[<>]' '{printf("-D%s=%s\n",$2,$3)}'
+}
+
+function parse_default_properties(){
+  cat ${1}/bookingapp-acceptance-test/src/test/resources/conf/acceptance_test_dev_system.properties \
+  | awk '{printf("-D%s\n", $0)}'
+}
+
+function parse_pom_xml(){
+  cat ${1}/bookingapp-acceptance-test/pom.xml \
+  | egrep "<(COOKIELESS_DOMAIN_ENABLED|LOCALISATION_DEV_LANGUAGE_TO_LOAD|UI_DEVELOPMENT_MODE_ENABLED)>" \
+  | awk -F'[<>]' '{printf("-D%s=%s\n",$2,$3)}'
+}
+
+function process_args(){
+  while [[ ${#} -gt 0 ]]; do
+    case "${1}" in
+    -v) DEBUG="true" ;;
+    -c) COPY_TO_CLIPBOARD="true" ;;
+    -h) print_help && exit 0 ;;
+    *) BA_HOME=${1} ;;
+    esac
+    shift
+  done
+
+}
+
+function main(){
+  local settings_from_maven settings_from_properties settings_from_pom_xml
+  echo "Collecting properties..."
+  echo -n "1. From maven settings..."
+  settings_from_maven=`parse_maven_settings $(get_maven_settings_file)`
+  echo " done"
+  debug_log "${settings_from_maven}"
+
+  echo -n "2. From project properties file..."
+  settings_from_properties=`parse_default_properties ${BA_HOME}`
+  echo " done"
+  debug_log "${settings_from_properties}"
+
+  echo -n "3. From project pom xml file..."
+  settings_from_pom_xml=`parse_pom_xml ${BA_HOME}`
+  echo " done"
+  debug_log "${settings_from_pom_xml}"
+
+  if [[ "${COPY_TO_CLIPBOARD}" == "true" ]]; then
+    echo -e "${settings_from_maven}\n${settings_from_properties}\n${settings_from_pom_xml}" > /dev/clipboard
+    echo "Result copied to the clipboard"
+  else
+    echo -e "\nJVM args:\n"
+    echo -e "${settings_from_maven}\n${settings_from_properties}\n${settings_from_pom_xml}"
+  fi
+}
+
+process_args "${@}"
+main
--- scripts/readme.md
+++ scripts/readme.md
@@ -1,4 +1,15 @@
-###btr 
+## Style Guide
+1. Please try to follow Google Shell Coding Style guide if possible
+    https://google.github.io/styleguide/shell.xml
+
+2. Please create help for scripts with -h option
+
+3. Please update this readme with a short description about your script and an example if needed
+
+###acceptance_test_jvm_args.sh
+Script to collect jvm args for Acceptance test execution from IDE.
+
+###btr
 Backup test result. Used to copy UI test result to an external directory which is hosted via http server
 
 ###get_confmail.sh
@@ -14,9 +25,9 @@ format tracelog into html (colors, collapsable, fancy)
 Python script to parse all module-descriptor in the specified directory and also extract descriptor files from jars.
 
 1. Usage: ```python ./module-desc.py c:\work\git\bookingapp```
-    
+
     print out all modules all dependecies
 
 2. Usage: ```python ./module-desc.py c:\work\git\bookingapp bookingapp-service-module-descriptor.xml```
-    
+
     print out the specified module dependecy tree
