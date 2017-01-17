cbb5c3d0969255d141ff7e1ad8c5ec9cecd76224
Laszlo Tarcsanyi <v-ltarcsanyi@expedia.com>
2017-01-17 16:56:55 +0100

    @noissue: add longest 5 task

--- scripts/build-time-analyzer.py
+++ scripts/build-time-analyzer.py
@@ -18,11 +18,16 @@ with urllib.request.urlopen(_url) as fetcher:
             timestamp = datetime.strptime("".join(parts[1:3]), "%d-%b-%Y%H:%M:%S")
             task = parts[5]
             target = parts[8]
-            tasks.append((task, target, timestamp))
+            tasks.append([task, target, timestamp])
 
-print("%10s | %20s | %s" % ('*' * 10, '*' * 20, '*' * 20))
+print("%10s | %30s | %s" % ('*' * 10, '*' * 30, '*' * 20))
 for i in range(len(tasks)):
     duration = timedelta()
     if len(tasks) - 1 > i:
         duration = tasks[i + 1][2] - tasks[i][2]
+    tasks[i].append(duration)
     print("%10s | %-30s | %s" % (duration, tasks[i][1], tasks[i][0]))
+
+print("\n\nLongest tasks:")
+for task in sorted(tasks, key=lambda a: a[3], reverse=True)[:5]:
+    print("%10s | %-30s | %s" % (task[3], task[1], task[0]))
