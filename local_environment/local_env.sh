6cc0a84eb8960a1613aaedb9127d50e8e24a38d0
Zsolt Saskovy <zsolt_saskovy@epam.com>
2019-07-19 11:07:22 +0200

    Checkito is now not starting if run with -no-stub

--- local_environment/local_env.sh
+++ local_environment/local_env.sh
@@ -198,6 +198,11 @@ function start-app {
 
     if [ "${APP}" == "checkito" ]
     then
+      if [ "${STUB_STATUS}" == "_no_stub" ]
+      then
+        return 1;
+      fi
+
       export SUIT=${SUIT}
     fi
 
